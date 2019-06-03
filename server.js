23219fd51e8db230e1124e3793905c34e52500bf
unknown <omer.moradd@gmail.com>
2019-06-03 23:10:24 +0300

    Changed syntax and notes

--- server.js
+++ server.js
@@ -2,103 +2,109 @@
 Our Setup - 
 Feel free to ignore all of this and skip to the questions at the end
 =======================================================*/
-var bodyParser = require('body-parser')
-var express = require('express')
-var app = express()
+const bodyParser = require( 'body-parser' )
+const express = require( 'express' )
+const app = express()
 
-var request = require('request')
-var mongoose = require('mongoose')
-var Book = require("./models/BookModel")
-var Person = require("./models/PersonModel")
+const request = require( 'request' )
+const mongoose = require( 'mongoose' )
+const Book = require( './models/BookModel' )
+const Person = require( './models/PersonModel' )
 
-mongoose.connect("mongodb://localhost/mongoose-practice")
+mongoose.connect( 'mongodb://localhost/mongoose-practice' )
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
 
 
 /*=====================================================
 Create books Collection
 =======================================================*/
-var isbns = [9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741, 9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154, 9780307455925, 9781501143519]
-var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+const isbns = [ 9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741, 9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154, 9780307455925, 9781501143519 ]
+const url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
 
-for (var i = 0; i < isbns.length; i++) {
-  var apiURL = url + isbns[i];
-  /*=====================================================
-  the first time you run your code, uncomment the function below.
-  for subsequent runs, re-comment it so that it runs only once!
-  that said, there is a fail-safe to avoid duplicates below  
-  =======================================================*/
-  loadFromAPI(apiURL)
+for ( let i = 0; i < isbns.length; i++ ) {
+    const apiURL = url + isbns[ i ]
+    /*=====================================================
+    the first time you run your code, uncomment the function below.
+    for subsequent runs, re-comment it so that it runs only once!
+    that said, there is a fail-safe to avoid duplicates below  
+    =======================================================*/
+    loadFromAPI( apiURL )
 }
-console.log("done");
+console.log( 'done' )
 
-function loadFromAPI(apiURL) {
+function loadFromAPI( apiURL ) {
 
-  request(apiURL, function(error, response, body) {
+    request( apiURL, function ( error, response, body ) {
 
-    var result = JSON.parse(body)
+        const result = JSON.parse( body )
 
-    if (result.totalItems && !error && response.statusCode == 200) {
-      var resBook = JSON.parse(body).items[0].volumeInfo
+        if ( result.totalItems && !error && response.statusCode === 200 ) {
+            const resBook = JSON.parse( body ).items[ 0 ].volumeInfo
 
-      var book = new Book({
-        title: resBook.title,
-        author: resBook.authors[0],
-        pages: resBook.pageCount,
-        genres: resBook.categories || ["Other"],
-        rating: resBook.averageRating || 5
-      })
+            const book = new Book( {
+                title: resBook.title,
+                author: resBook.authors[ 0 ],
+                pages: resBook.pageCount,
+                genres: resBook.categories || [ 'Other' ],
+                rating: resBook.averageRating || 5
+            } )
 
-      //Only save if the book doesn't exist yet
-      Book.findOne({ title: book.title }, function(err, foundBook) {
-        if (!foundBook) {
-          book.save()
+            //Only save if the book doesn't exist yet
+            Book.findOne( { title: book.title }, function ( err, foundBook ) {
+                if ( !foundBook ) {
+                    book.save()
+                }
+            } )
         }
-      })
-    }
-  })
+    } )
 }
 
 
 /*=====================================================
 Create People Collection
 =======================================================*/
-var colors = ["brown", "black", "red", "yellow", "green", "grey"]
-var getColor = function() {
-  return colors[Math.floor(Math.random() * colors.length)]
-}
-var getWeight = function() {
-  return getRandIntBetween(50, 120)
-}
-var getHeight = function() {
-  return getRandIntBetween(120, 230)
-}
-var getSalary = function() {
-  return getRandIntBetween(20000, 50000)
-}
-var getNumKids = function() {
-  return Math.floor(Math.random() * 3)
+const colors = [ 'brown', 'black', 'red', 'yellow', 'green', 'grey' ]
+
+const getColor = function () {
+    return colors[ Math.floor( Math.random() * colors.length ) ]
 }
 
-var getRandIntBetween = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+const getWeight = function () {
+    return getRandIntBetween( 50, 120 )
 }
 
-var getKids = function(numKids) {
-  var kids = [];
-  for (var i = 0; i < numKids; i++) {
-    kids.push({
-      hair: getColor(),
-      eyes: getColor(),
-      weight: getWeight(),
-      height: getHeight(),
-    })
-  }
-  return kids;
+const getHeight = function () {
+    return getRandIntBetween( 120, 230 )
 }
 
+const getSalary = function () {
+    return getRandIntBetween( 20000, 50000 )
+}
+
+const getNumKids = function () {
+    return Math.floor( Math.random() * 3 )
+}
+
+const getRandIntBetween = function ( min, max ) {
+    return Math.floor( Math.random() * ( max - min + 1 ) + min )
+}
+
+const getKids = function ( numKids ) {
+    const kids = []
+
+    for ( let i = 0; i < numKids; i++ ) {
+        kids.push( {
+            hair: getColor(),
+            eyes: getColor(),
+            weight: getWeight(),
+            height: getHeight(),
+        } )
+    }
+
+    return kids
+}
 
 /*=====================================================
 the below code always makes sure
@@ -108,34 +114,30 @@ adds new people and their kids until you do have 100
 try to understand how this code works
 could you write it differently?
 =======================================================*/
-Person.find({}).count(function(err, count) {
-  // the below two loops could be changed to a simple:
-  // for (var i = count; i < 100; i++) {}
-  if (count < 100) {
-    for (var i = 0; i < 100 - count; i++) {
-      var numKids = getNumKids();
-      var p = new Person({
-        hair: getColor(),
-        eyes: getColor(),
-        weight: getWeight(),
-        height: getHeight(),
-        salary: getSalary(),
-        numKids: numKids,
-        kids: getKids(numKids)
-      });
-      p.save();
+Person.find( {} ).count( function ( err, count ) {
+    // the below two loops could be changed to a simple:
+    // for ( let i = count; i < 100; i++ ) {}
+    if ( count < 100 ) {
+        for ( let i = 0; i < 100 - count; i++ ) {
+            const numKids = getNumKids()
+
+            const p = new Person( {
+                hair: getColor(),
+                eyes: getColor(),
+                weight: getWeight(),
+                height: getHeight(),
+                salary: getSalary(),
+                numKids: numKids,
+                kids: getKids( numKids )
+            } )
+
+            p.save()
+        }
     }
-  }
-})
+} )
 
-
-/*=====================================================
-Start the server:
-=======================================================*/
-
-app.listen(3000, function() {
-  console.log("Server up and running on port 3000")
-})
+// Start the server
+app.listen( 3000, () => console.log( 'Server up and running on port 3000' ) )
 
 
 /*=====================================================
